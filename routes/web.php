ca46dd21607dcfb5f680566db7a9b093946afec9
kenta.fujiwara <ken.noro3@gmail.com>
2022-02-08 08:12:57 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,14 @@ Route::get('/', function () {
 
 Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup.get');
 
-Route::post('sinup','Auth\RegisterController@register')->name('singup.post');
+Route::post('signup','Auth\RegisterController@register')->name('signup.post');
+
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
 
 //->name()はルーティングに名前をつけている
